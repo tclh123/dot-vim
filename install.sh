e8c58653dab25bcd7e88f73e93814b8121754abb
lihan <tclh123@gmail.com>
2018-07-09 11:41:29 +0800

    try backup original .vim

--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@ set -e
 here="$(cd "${0%/*}"; pwd)"
 
 # symlink .vim/
+if [[ -d "${HOME}/.vim" ]]; then
+  mv "${HOME}/.vim" "${HOME}/.vim.orig"
+fi
 ln -sfnv ${here#$HOME/} "${HOME}/.vim"
 
 # symlink .vimrc
